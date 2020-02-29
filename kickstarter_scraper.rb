0f1b0a9448d603f856380356a3ffd70e17d828e8
Samantha Meason <sbmeason@gmail.com>
2020-02-29 21:46:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,13 +3,15 @@
 #title: project.css("h2.bbcard_name strong a").text 
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
 require 'nokogiri'
 require 'pry'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
-  binding.pry
+  
 end
 
 create_project_hash
